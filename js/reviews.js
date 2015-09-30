e9d1d766ef5d1f5ce25197f615a7d493ee7b1ba1
Igor Antonov <a@iantonov.me>
2015-09-30 22:46:33 +1000

    Добавил загрузку по Ajax

--- js/reviews.js
+++ js/reviews.js
@@ -8,6 +8,14 @@
    '5': 'review-rating-five'
  };
 
+ var readyState = {
+   'UNSENT': 0,
+   'OPENED': 1,
+   'HEADERS_RECEIVED': 2,
+   'LOADING': 3,
+   'DONE': 4
+ };
+
  var REQUEST_FAILTURE_TIMEOUT = 10000;
 
  //Контейнер для помещения списка отзывов
@@ -18,7 +26,9 @@
  showHideBlock(reviewsFilter, false);
 
  //Рендеринг списка отзывов
- renderReviews(reviews);
+ loadReviewsList(function(loadedReviews) {
+   renderReviews(loadedReviews);
+ });
 
  //Вновь отображаем блок с фильтрами
  showHideBlock(reviewsFilter, true);
@@ -63,12 +73,74 @@
 
  };
 
+//Загрузка списка отзывов Ajax
+function loadReviewsList(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.timeout = REQUEST_FAILTURE_TIMEOUT;
+  xhr.open('get', 'data/reviews.json');
+  xhr.send();
+
+  xhr.onreadystatechange = function(evt) {
+    var loadedXhr = evt.target;
+
+    switch (loadedXhr.readyState) {
+      case readyState.OPENED:
+      case readyState.HEADERS_RECEIVED:
+      case readyState.LOADING:
+        reviewsContainer.classList.add('reviews-list-loading');
+        break;
+      case readyState.DONE:
+      default:
+        if (xhr.status === 200) {
+          var receivedData = loadedXhr.response;
+          reviewsContainer.classList.remove('reviews-list-loading');
+          callback(JSON.parse(receivedData));
+        }
+
+        if (xhr.status > 400) {
+          showLoadFailture();
+        }
+        break;
+    }
+  };
+
+  xhr.ontimeout = function() {
+    showLoadFailture();
+  };
+
+};
+
+ function showLoadFailture() {
+   reviewsContainer.classList.add('reviews-load-failure')
+ }
+
+
+
+ function setFilterToReviews(reviewsToFilter, filterName) {
+   var filteredReviews = reviewsToFilter.slice(0);
+   switch (filterName) {
+     case 'reviews-good':
+
+
+       break;
+     default:
+
+   }
+ }
+
+
  function showHideBlock(element, visible) {
    !visible ?
     element.classList.add('invisible') :
     element.classList.remove('invisible');
 
     return element;
+ }
+
+ function initFilters() {
+   var filtersContainer = document.querySelector('.reviews-filter');
+
+   //todo написать инициализацию фильтров
  }
 
 })();
