598d92682d4c4702d8eb14d30a0f520a15cedcea
Igor Antonov <a@iantonov.me>
2015-10-20 22:52:10 +1000

    Fixes
    - Fix. Работа параллакса в FF
    - Fix. Оптимизация отлова события scroll

--- js/game_demo.js
+++ js/game_demo.js
@@ -3,13 +3,14 @@
   var TIMEOUT_FOR_PARALLAX = 100;
   var headerCloudsContainer = document.querySelector('.header-clouds');
 
+  changeCloudsPosition();
   initScroll();
 
   function initScroll() {
     var someTimeout;
     window.addEventListener('scroll', function() {
         clearTimeout(someTimeout);
-        someTimeout = setTimeout(window.dispatchEvent(new CustomEvent('hideBlockWithColouds')), TIMEOUT_FOR_PARALLAX);
+        someTimeout = setTimeout(checkCloudPosition, TIMEOUT_FOR_PARALLAX);
     });
 
     window.addEventListener('hideBlockWithColouds', function() {
@@ -17,35 +18,28 @@
     })
   }
 
-  function changeCloudsPosition() {
-    if (!isVisiblePosition()) {
-      return;
+  function checkCloudPosition() {
+    if  (isVisiblePosition()) {
+      window.dispatchEvent(new CustomEvent('hideBlockWithColouds'));
     }
+  }
 
+  function changeCloudsPosition() {
     changeBackgroundPosition(headerCloudsContainer, getChordsForClouds());
   }
 
   function changeBackgroundPosition(element, newPositionX) {
-    element.style.backgroundPositionX = newPositionX + 'px';
+    element.style.backgroundPosition = newPositionX + 'px';
   }
 
   function isVisiblePosition() {
     var result = headerCloudsContainer.getBoundingClientRect().bottom > 0;
-
-    if (!result) {        
-        window.dispatchEvent(new CustomEvent('parallaxNeeded'));
-    }
-
     return result;
-
   }
 
   function getChordsForClouds() {
     var STEP = 100;
     return headerCloudsContainer.getBoundingClientRect().bottom - STEP;
   }
-
-
-
 
 })();
